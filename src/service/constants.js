5fb12dbfc185fe7fe31f6656cb0640c61ff0ae81
unknown <only-666@mail.ru>
2021-04-18 11:45:42 +0300

    module2-task3 добавил веб сервер

--- src/service/constants.js
+++ src/service/constants.js
@@ -5,9 +5,17 @@ const USER_ARGV_INDEX = 2;
 const ExitCode = {
   success: 0
 };
+const HTTP_CODES = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401
+};
 
 module.exports = {
   DEFAULT_COMMAND,
   USER_ARGV_INDEX,
-  ExitCode
+  ExitCode,
+  HTTP_CODES
 };
