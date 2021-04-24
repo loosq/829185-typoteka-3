8f2ea262cafbb5f0994bcfaa719cdc96cfbde833
sergei <only-666@mail.ru>
2021-04-24 22:47:04 +0300

    module3-task2 добавил шаблонизатор и вьюхи по некоторым роутам

--- src/express/express.js
+++ src/express/express.js
@@ -1,17 +1,30 @@
 'use strict';
 
 const express = require(`express`);
+const path = require(`path`);
 const port = 8080;
+const PUBLIC_DIR = `public`;
 const app = express();
 const myRoutes = require(`../routes/my`);
 const articlesRoutes = require(`../routes/articles`);
 
-app.get(`/`, (req, res) => res.send(`/`));
-app.get(`/register`, (req, res) => res.send(`/register`));
-app.get(`/login`, (req, res) => res.send(`/login`));
-app.get(`/search`, (req, res) => res.send(`/search`));
-app.get(`/categories`, (req, res) => res.send(`/categories`));
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+app.get(`/`, (req, res) => res.render(`main.pug`));
+app.get(`/login`, (req, res) => res.render(`sign-in.pug`));
+app.get(`/sing-up`, (req, res) => res.render(`registration.pug`));
+app.get(`/search-1`, (req, res) => res.render(`search-1.pug`));
+app.get(`/post`, (req, res) => res.render(`post-user.pug`));
 app.use(`/my`, myRoutes);
 app.use(`/articles`, articlesRoutes);
+
+app.use((req, res) => res.status(400).render(`errors/404`));
+app.use((err, req, res, next) => {
+  res.status(500).render(`errors/500`);
+  next();
+});
+
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 
 app.listen(port);
--- src/routes/my.js
+++ src/routes/my.js
@@ -3,7 +3,7 @@
 const {Router} = require(`express`);
 const myRouter = new Router();
 
-myRouter.get(`/`, (req, res) => res.send(`/my`));
+myRouter.get(`/`, (req, res) => res.render(`admin-publications.pug`));
 myRouter.get(`/comments`, (req, res) => res.send(`/my/comments`));
 
 module.exports = myRouter;
