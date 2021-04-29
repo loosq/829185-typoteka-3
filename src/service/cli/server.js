ca5460bbee2469bc65f2ae9479c58de839df072f
sergei <only-666@mail.ru>
2021-04-29 09:29:10 +0300

    module3-task3 решил конфликты, убрал лишнее

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -3,63 +3,38 @@
 const {HTTP_CODES} = require(`../constants`);
 const {readFile} = require(`fs/promises`);
 const chalk = require(`chalk`);
-const http = require(`http`);
 const DEFAULT_SERVER_PORT = 3000;
 const FILENAME = `mocks.json`;
 
-const onClientConnect = async (req, res) => {
-  const notFoundMessageText = `Not found`;
+const express = require(`express`);
+const app = express();
 
-  switch (req.url) {
-    case `/`:
-      try {
-        const fileContent = await readFile(FILENAME);
-        const mocks = JSON.parse(fileContent);
-        const message = mocks.map((post) => `<li>${post.title}</li>`).join(``);
-        sendResponse(res, HTTP_CODES.OK, `<ul>${message}</ul>`);
-      } catch (err) {
-        sendResponse(res, HTTP_CODES.NOT_FOUND, notFoundMessageText);
-      }
-
-      break;
-    default:
-      sendResponse(res, HTTP_CODES.NOT_FOUND, notFoundMessageText);
-      break;
+app.get(`/posts`, async (req, res) => {
+  try {
+    const content = await readFile(FILENAME);
+    const mocks = JSON.parse(content);
+    res.json(mocks);
+  } catch (err) {
+    console.error(chalk.red(`Что то пошло не так:`, err));
+    res.status(HTTP_CODES.INTERNAL_SERVER_ERROR).send(err);
   }
-};
+});
 
-const sendResponse = (res, statusCode, message) => {
-  const template = `
-    <!Doctype html>
-      <html lang="ru">
-      <head>
-        <title>Some random post</title>
-      </head>
-      <body>${message}</body>
-    </html>`.trim();
-
-  res.statusCode = statusCode;
-  res.writeHead(statusCode, {
-    'Content-Type': `text/html; charset=UTF-8`,
-  });
-
-  res.end(template);
-};
+app.use(express.json());
+app.use((req, res) => {
+  res.status(HTTP_CODES.NOT_FOUND).send(`Not found`);
+});
 
 module.exports = {
   name: `--server`,
   async run(args) {
     const [customPort] = args;
     const serverPort = Number.parseInt(customPort, 10) || DEFAULT_SERVER_PORT;
-
-    http.createServer(onClientConnect)
-      .listen(serverPort)
-      .on(`listening`, (err) => {
-        if (err) {
-          return console.error(chalk.red(`Ошибка при создании сервера`, err));
-        }
-
-        return console.info(chalk.green(`Ожидаю соединений на ${serverPort}`));
-      });
+    app.listen(serverPort, (err) => {
+      if (err) {
+        return console.error(chalk.red(`Ошибка при создании сервера`, err));
+      }
+      return console.info(chalk.green(`Ожидаю соединений на ${serverPort}`));
+    });
   }
 };
