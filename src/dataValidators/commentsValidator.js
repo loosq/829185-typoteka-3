a79014675eb243d9f19ac2f7ab09e5d801d92aee
sergei <only-666@mail.ru>
2021-06-20 11:24:04 +0300

    module4-task2 добавил правки по ревью

--- src/dataValidators/commentsValidator.js
+++ src/dataValidators/commentsValidator.js
@@ -4,8 +4,9 @@ const {HTTP_CODES} = require(`../service/constants`);
 const commentKey = `text`;
 
 module.exports = (req, res, next) => {
+  const hasCommentText = commentKey in req.body && req.body[commentKey].length;
 
-  if (!(commentKey in req.body) || !req.body[commentKey].length) {
+  if (!hasCommentText) {
     res.status(HTTP_CODES.BAD_REQUEST).send(`Bad request`);
   }
 
--- src/service/api/articles.e2e.test.js
+++ src/service/api/articles.e2e.test.js
@@ -19,7 +19,8 @@ const routes = initApi(validArticles);
 const apiArticles = `/api/articles`;
 app.use(`/api`, routes);
 
-describe(`Articles API end-points`, () => {
+
+describe(`Articles API end-points, method GET`, () => {
   test(`When get /api/articles valid request, res code should be 200`, async () => {
     const res = await request(app).get(`${apiArticles}`);
     expect(res.statusCode).toBe(HTTP_CODES.OK);
@@ -30,6 +31,18 @@ describe(`Articles API end-points`, () => {
     expect(Array.isArray(res.body)).toBeTruthy();
   });
 
+  test(`When get /api/articles/:id/comments, res code should be 200`, async () => {
+    const res = await request(app).get(`${apiArticles}/${validArticles[0].id}/comments`);
+    expect(res.statusCode).toBe(HTTP_CODES.OK);
+  });
+
+  test(`When get /api/articles/:id/comments, res body should be an array`, async () => {
+    const res = await request(app).get(`${apiArticles}/${validArticles[0].id}/comments`);
+    expect(Array.isArray(res.body)).toBeTruthy();
+  });
+});
+
+describe(`Articles API end-points, method POST`, () => {
   test(`When post /api/articles valid article, res code should be 201`, async () => {
     const res = await request(app).post(`${apiArticles}`).send(validArticle);
     expect(res.statusCode).toBe(HTTP_CODES.CREATED);
@@ -40,6 +53,23 @@ describe(`Articles API end-points`, () => {
     expect(res.statusCode).toBe(HTTP_CODES.BAD_REQUEST);
   });
 
+  test(`When post /api/articles/:id/comments/:commentId valid comment, res code should be 200`, async () => {
+    const res = await request(app).post(`${apiArticles}/${validArticles[0].id}/comments`).send(validComment);
+    expect(res.statusCode).toBe(HTTP_CODES.CREATED);
+  });
+
+  test(`When post /api/articles/:id/comments/:commentId invalid comment, res code should be 400`, async () => {
+    const res = await request(app).post(`${apiArticles}/${validArticles[0].id}/comments`).send(invalidComment);
+    expect(res.statusCode).toBe(HTTP_CODES.BAD_REQUEST);
+  });
+
+  test(`When post /api/articles/:id/comments/:commentId empty comment, res code should be 400`, async () => {
+    const res = await request(app).post(`${apiArticles}/${validArticles[0].id}/comments`).send(emptyComment);
+    expect(res.statusCode).toBe(HTTP_CODES.BAD_REQUEST);
+  });
+});
+
+describe(`Articles API end-points, method PUT`, () => {
   test(`When put /api/articles/:id valid request, res code should be 200`, async () => {
     const res = await request(app).put(`${apiArticles}/${validArticles[0].id}`).send(validArticleAttr);
     expect(res.statusCode).toBe(HTTP_CODES.OK);
@@ -49,6 +79,10 @@ describe(`Articles API end-points`, () => {
     const res = await request(app).put(`${apiArticles}/${validArticles[0].id}`).send(invalidArticleAttr);
     expect(res.statusCode).toBe(HTTP_CODES.BAD_REQUEST);
   });
+});
+
+
+describe(`Articles API end-points, method DELETE`, () => {
 
   test(`When delete /api/articles/:id valid request, res code should be 200`, async () => {
     const res = await request(app).delete(`${apiArticles}/${validArticles[2].id}`);
@@ -67,16 +101,6 @@ describe(`Articles API end-points`, () => {
     expect(res.statusCode).toBe(HTTP_CODES.BAD_REQUEST);
   });
 
-  test(`When get /api/articles/:id/comments, res code should be 200`, async () => {
-    const res = await request(app).get(`${apiArticles}/${validArticles[0].id}/comments`);
-    expect(res.statusCode).toBe(HTTP_CODES.OK);
-  });
-
-  test(`When get /api/articles/:id/comments, res body should be an array`, async () => {
-    const res = await request(app).get(`${apiArticles}/${validArticles[0].id}/comments`);
-    expect(Array.isArray(res.body)).toBeTruthy();
-  });
-
   test(`When delete /api/articles/:id/comments/:commentId valid id, res body should be 200`, async () => {
     const res = await request(app).delete(`${apiArticles}/${validArticles[3].id}/comments/${validArticles[3].comments[0].id}`);
     expect(res.statusCode).toBe(HTTP_CODES.OK);
@@ -84,21 +108,6 @@ describe(`Articles API end-points`, () => {
 
   test(`When delete /api/articles/:id/comments/:commentId invalid article id, res body should be 400`, async () => {
     const res = await request(app).delete(`${apiArticles}/some_invalid_article_id/comments/${validArticles[3].comments[0].id}`);
-    expect(res.statusCode).toBe(HTTP_CODES.BAD_REQUEST);
-  });
-
-  test(`When post /api/articles/:id/comments/:commentId valid comment, res code should be 200`, async () => {
-    const res = await request(app).post(`${apiArticles}/${validArticles[0].id}/comments`).send(validComment);
-    expect(res.statusCode).toBe(HTTP_CODES.CREATED);
-  });
-
-  test(`When post /api/articles/:id/comments/:commentId invalid comment, res code should be 400`, async () => {
-    const res = await request(app).post(`${apiArticles}/${validArticles[0].id}/comments`).send(invalidComment);
-    expect(res.statusCode).toBe(HTTP_CODES.BAD_REQUEST);
-  });
-
-  test(`When post /api/articles/:id/comments/:commentId empty comment, res code should be 400`, async () => {
-    const res = await request(app).post(`${apiArticles}/${validArticles[0].id}/comments`).send(emptyComment);
     expect(res.statusCode).toBe(HTTP_CODES.BAD_REQUEST);
   });
 });
